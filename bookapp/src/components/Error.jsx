d761ba90e43c32d12d6fc8067f475ad2e11067b4
geonho <0531a18@gmail.com>
2025-05-27 18:03:55 +0900

    0527 Book 페이지 구조

---
+++ bookapp/src/components/Error.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div className="text-center text-2xl">
+      <div className="mt-4 text-6xl">🚨</div>
+      Error!
+    </div>
+  );
+};
+
+export default Error;
---
+++ bookapp/src/pages/BookDetails.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import EditBook from "./EditBook";
+
+const BookDetails = () => {
+  return (
+    <div>
+      <h1 className="text-[32px]">Book Details</h1>
+      <EditBook />
+    </div>
+  );
+};
+
+export default BookDetails;
